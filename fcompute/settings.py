6c7ff2ebaee2777246f1a8e4fc662959a0169056
harshitgarg <harshitgargmnit@gmail.com>
2019-04-27 20:51:52 +0000

    Add whitenoise settings

--- fcompute/settings.py
+++ fcompute/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fcompute.urls'
@@ -135,4 +136,4 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_REDIRECT_URL = 'home'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
